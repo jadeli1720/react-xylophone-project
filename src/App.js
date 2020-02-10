f0fb70ea12bf60b0b7427337e55ea481b7522ad5
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-02-09 21:34:59 -0700

    Creating NoteButton component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import notes from './assets/notes.js';
 import './App.scss';
 
+
+// Making NoteButton component. Can separate this later into it's own file
+const NoteButton = (props) => {
+  console.log("PROPS", props)
+  return (
+    <button onClick={() => new Audio(notes[0].file).play() } >
+      {props.note.name}
+    </button>
+  )
+}
+
 function App() {
   return (
     <div className="page">
       <h1>Create your react xylophone</h1>
       <div className="xylophone">
+        <NoteButton note={notes[0]} />
         {/* I am placeholder buttons, please create me using a map, and with your own component */}
-        <button onClick={() => new Audio(notes[0].file).play() }>c</button>
+        {/* <button onClick={() => new Audio(notes[0].file).play() }>c</button> */}
         <button>d1</button>
         <button>e1</button>
         <button>f</button>
